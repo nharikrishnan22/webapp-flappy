112daf9ff51ef2294a2e08bcad101d8fe9e96b50
nharikrishnan22 <nhkrishnan4@gmail.com>
2016-08-09 13:51:54 +0100

    latest update 1470747113842

--- js/interactivity.js
+++ js/interactivity.js
@@ -3,11 +3,11 @@ jQuery("#High Scores").on("click", function registerScore() {
   console.log(highscoreList);
   gameResult = {player: playerName, score: score};
   highscoreList.push(gameResult);
-  highscoreList.sort(function(a,b) { return (b.score - a.score ) });
+  highscoreList.sort(function(a,b) { return (b.score - a.score ); });
   if (score == highscoreList[0].score && score>1) {
     jQuery("#content").empty();
     playerName = prompt("You got a high score! What is your name?");
-    jQuery("#content").append(
+    jQuery("#content").css('color', 'yellow').append(
       "<ul>" +
         "<li>"+ playerName + ":" + score + "<li>" +
       "</ul>");
@@ -18,14 +18,14 @@ jQuery("#High Scores").on("click", function registerScore() {
 jQuery("#credits").on("click", function() {
   var message = "Game created by Neeraj";
   jQuery("#content").empty();
-  jQuery("#content").append(
+  jQuery("#content").css('color', 'yellow').append(
     "<div>"+ message + "</div>"
   );
 });
 
 jQuery("#help").on("click", function() {
   jQuery("#content").empty();
-  jQuery("#content").append(
+  jQuery("#content").css('color', 'yellow').append(
     "<ol>" +
           "<li>" + "Press SPACE to go up" + "</li>" +
           "<li>" + "Avoid the incoming pipes" + "</li>" +
@@ -38,13 +38,13 @@ function registerScore(score) {
   console.log(highscoreList);
   gameResult = {player: playerName, score: score};
   highscoreList.push(gameResult);
-  highscoreList.sort(function(a,b) { return (b.score - a.score ) });
+  highscoreList.sort(function(a,b) { return (b.score - a.score ); });
   if (score == highscoreList[0].score && score>1) {
     jQuery("#content").empty();
     playerName = prompt("You got a high score! What is your name?");
-    jQuery("#content").append(
+    jQuery("#content").css('colour', 'yellow').append(
       "<ul>" +
         "<li>"+ playerName + ":" + score + "<li>" +
-      "</ul>")
+      "</ul>");
   }
 }
